074831328c06a57e6a5cabf446ef32a8523fc3a8
StefanoPruna <48112000+StefanoPruna@users.noreply.github.com>
2020-09-16 16:01:33 +1000

    Classes
    Inheritence classes

---
+++ My Effect Team/Assets/Scripts/Spell.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Spell
+{
+    public string name;
+    public int levelRequired;
+    public int itemIdRequired;
+    public int expGained;
+
+    public Spell(string name, int levRequired, int itemRequired, int exp)
+    {
+        this.name = name;
+        this.levelRequired = levRequired;
+        this.itemIdRequired = itemRequired;
+        this.expGained = exp;
+    }
+
+    public void Cast()
+    {
+        Debug.Log("Casting " + this.name);
+    }
+}
---
+++ My Effect Team/Assets/Wizard.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wizard : MonoBehaviour
+{
+    /*public Spell fireBall;
+
+    public int level = 1;
+    public int exp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireBall = new Spell("Fire Ball", 1, 27, 35);//35 stands for experience 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            //GetComponent<Spell>().Cast();
+            fireBall.Cast();
+            exp += fireBall.expGained;
+        }
+    }*/
+
+    //alternative method:
+    public Spell[] spells;
+
+    public int level = 1;
+    public int exp;
+
+    public void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            foreach (var spell in spells)
+            {
+                if (spell.levelRequired == level)
+                {
+                    spell.Cast();
+                    exp += spell.expGained;           
+                    
+                }
+            }
+        }
+    }
+}
