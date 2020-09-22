6269674de4e49e581e8ffba693ff7b67efd64f22
StefanoPruna <48112000+StefanoPruna@users.noreply.github.com>
2020-09-22 15:56:22 +1000

    Lists
    Difference between Arrays and Lists

--- My Effect Team/Assets/Scripts/GameManager.cs
+++ My Effect Team/Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    //PROPERTIES
     /*private bool isGameOver;
     public bool IsGameOver
     {
@@ -21,48 +22,91 @@ public class GameManager : MonoBehaviour
         }
     }*/
 
-        //Cleaner way to do the Auto Properties/above:
-        public bool IsGameOver { get; private set; }
-        
-        public bool IsSpeedOn { get; private set; }        
-    public string myName { get; set; }
-    public string _myName;
+    //    //Cleaner way to do the Auto Properties/above:
+    //    public bool IsGameOver { get; private set; }
 
-    float Speed { get; set; }
-    private float _speed;
+    //    public bool IsSpeedOn { get; private set; }        
+    //public string myName { get; set; }
+    //public string _myName;
 
-    
+    //float Speed { get; set; }
+    //private float _speed;
 
 
 
-    // Start is called before the first frame update
-    void Start()
+
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    //isGameOver = false;
+
+    //    IsGameOver = false;
+    //    if (IsGameOver == true)
+    //    {
+
+    //    }
+
+    //    IsSpeedOn = false;
+    //    _myName = "Loske";
+
+    //    _speed = 10f;
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        IsGameOver = true;
+    //        Debug.Log("You lose! Game Over!");
+    //        IsSpeedOn = true;
+    //        Debug.Log(myName + ", You are Speeding!" + Speed);
+
+    //    }
+    //}END OF PROPERTIES
+
+
+    /*Creating Lists:
+    public List<GameObject> enemiesToSpawn = new List<GameObject>();//LISTS ARE DYNAMIC
+    public GameObject[] objectsToSpawn = new GameObject[10];//ARRAYS ARE STATIC, YOU HAVE TO PROVIDE THE NUMBER
+
+    private void Start()
     {
-        //isGameOver = false;
+        objectsToSpawn[0] = new GameObject();
+        enemiesToSpawn.Add(new GameObject());
+        //ACCESS OR INTERACT WITH DATA IN THE SAME WAY
+        objectsToSpawn[2].name = "John";
+        enemiesToSpawn[4].name = "Loske";
+    }*/
 
-        IsGameOver = false;
-        if (IsGameOver == true)
-        {
-
-        }
-
-        IsSpeedOn = false;
-        _myName = "Loske";
-
-        _speed = 10f;
+    public List<string> someNames = new List<string>();
         
+    private void Start()
+    {        
+        //foreach(var names in someNames)
+        //{
+        //    Debug.Log(names);
+        //}               
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            IsGameOver = true;
-            Debug.Log("You lose! Game Over!");
-            IsSpeedOn = true;
-            Debug.Log(myName + ", You are Speeding!" + Speed);
+            var nameToRemove = someNames[Random.Range(0, someNames.Count)];
+            someNames.Remove(nameToRemove);
 
+            //someNames.Remove(someNames[Random.Range(0, someNames.Count)]);
+
+            foreach (var newName in someNames)
+                {            
+                    Debug.Log(newName);
+                }
+
+            Debug.Log("We remove the following name:" + nameToRemove);            
         }
     }
 }
---
+++ My Effect Team/Assets/Scripts/List.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class List : MonoBehaviour
+{
+    public List<GameObject> somePlayers = new List<GameObject>();
+    public List<string> fewPlayers = new List<string>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //foreach (var player in somePlayers)
+        //{
+        //    Debug.Log(player);
+        //}     
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            fewPlayers.Remove(fewPlayers[Random.Range(0, fewPlayers.Count)]);
+
+            foreach (var newPlayer in fewPlayers)
+                {                
+                    Debug.Log(newPlayer);
+                }   
+        }
+        
+    }
+
+    
+}
