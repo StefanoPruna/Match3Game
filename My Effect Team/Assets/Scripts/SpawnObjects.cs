21507f90559486b2c9b9d5225ac943f0b05f04dd
StefanoPruna <48112000+StefanoPruna@users.noreply.github.com>
2020-09-23 16:16:27 +1000

    Build Object list from an Array
    Challange:
    
    Build a list of objects from an Array;
    Add one object at the time to a max of 10;
    Once created and added 10 objects to the inspector/Unity, change the color of all the objects and clear the list

---
+++ My Effect Team/Assets/Scripts/SpawnObjects.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour
+{
+    public List<GameObject> generateObj = new List<GameObject>();
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            GenerateObjects();
+            ChangePosition();
+        }
+        
+    }
+
+    public void ChangePosition()
+    {
+        transform.position = new Vector3(Random.Range(0, generateObj.Count), Random.Range(0, generateObj.Count), Random.Range(0, generateObj.Count));
+    }
+
+    public void GenerateObjects()
+    {
+        GameObject[] createEnemies = GameObject.FindGameObjectsWithTag("Enemy");       
+        generateObj.Add(new GameObject(tag = "Enemy"));
+
+        if (generateObj.Count > 10)
+        {
+            foreach (var c in createEnemies)
+            {
+                c.GetComponent<MeshRenderer>().material.color = Color.green;
+                generateObj.Clear();
+            }
+        }
+    }
+}
---
+++ My Effect Team/Assets/Scripts/SpawnObjectsChallange.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjectsChallange : MonoBehaviour
+{
+    public GameObject[] createObjects = new GameObject[3];//Array
+    public List<GameObject> objectCreated = new List<GameObject>();//List
+    public int SpawnCount { get; set; }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+                                                                            
+    // Update is called once per frame
+    void Update()
+    {
+        if (SpawnCount > 10)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            CreateObjects();
+            EndOfProject();
+        }               
+    }
+
+    void CreateObjects()
+    {
+        var objectToSpawn = createObjects[Random.Range(0, createObjects.Length)];
+        objectToSpawn = Instantiate(objectToSpawn, new Vector3(Random.Range(-10, createObjects.Length), Random.Range(-10, createObjects.Length), Random.Range(-10, createObjects.Length)), Quaternion.identity) as GameObject;
+
+        objectCreated.Add(objectToSpawn);
+
+        SpawnCount++;
+    }
+
+    void EndOfProject()
+    {
+        if (SpawnCount > 10)
+        {
+            foreach (var obj in objectCreated)
+            {
+                obj.GetComponent<MeshRenderer>().material.color = Color.green;
+            }
+
+            objectCreated.Clear();
+        }
+    }
+}
